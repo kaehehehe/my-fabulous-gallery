c628dfc6b2bb6d6054a419abbf7d4b23b10a1761
Kae <kaehehehe@gmail.com>
2025-01-28 11:21:12 +0900

    chore: remove displayed models

--- src/components/Artwork.jsx
+++ src/components/Artwork.jsx
@@ -1,12 +1,8 @@
-import { useGLTF } from "@react-three/drei";
-import React, { useEffect } from "react";
+// import { useGLTF } from "@react-three/drei";
+// import React, { useEffect } from "react";
 import * as THREE from "three";
 
 export function Artwork({ rotationY }) {
-  const iceCreamTexture = new THREE.TextureLoader().load(
-    "/textures/ice-cream.jpg"
-  );
-
   const textures = [
     new THREE.TextureLoader().load("/textures/box.jpg"),
     new THREE.TextureLoader().load("/textures/box.jpg"),
@@ -15,15 +11,6 @@ export function Artwork({ rotationY }) {
     new THREE.TextureLoader().load("/textures/box.jpg"),
     new THREE.TextureLoader().load("/textures/box.jpg"),
   ];
-
-  const { scene } = useGLTF("ice-cream.glb");
-
-  useEffect(() => {
-    scene.children[2].material.color.set("#DAB27A");
-    scene.children[4].material.map = iceCreamTexture;
-    scene.children[4].material.needsUpdate = true;
-    THREE.ColorManagement.enabled = true;
-  }, [scene]);
 
   return (
     <group rotation-y={rotationY}>
@@ -38,9 +25,9 @@ export function Artwork({ rotationY }) {
         ))}
       </mesh>
 
-      <mesh position={[0, -0.63, -3.85]}>
-        <primitive object={scene.clone()} scale={[0.07, 0.07, 0.07]} />
-      </mesh>
+      {/* <mesh position={[0, -0.63, -3.85]}>
+        <primitive object={scene.clone()} scale={[0.7, 0.7, 0.7]} />
+      </mesh> */}
     </group>
   );
 }
