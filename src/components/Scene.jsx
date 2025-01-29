55cb572848fb2bc3215b8041214f1afcb89e09e3
Kae <kaehehehe@gmail.com>
2025-01-29 20:22:17 +0900

    feat: add new models

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -4,45 +4,52 @@ import { Floor } from "./Floor";
 import { useFrame } from "@react-three/fiber";
 import { useGLTF } from "@react-three/drei";
 
-const MODEL_COUNT = 5;
+const MODEL_COUNT = 6;
 
 export function Scene({ setPreviewLink }) {
   const { scene: magnemiteScene } = useGLTF("magnemite.glb");
   const { scene: lipstickScene } = useGLTF("lipstick.glb");
   const { scene: iceCreamScene } = useGLTF("soft-serve-ice-cream.glb");
-  const { scene: strawberryScene } = useGLTF("strawberry.glb");
+  const { scene: retroPhone } = useGLTF("retro-phone.glb");
   const { scene: starCandyScene } = useGLTF("star-candy.glb");
+  const { scene: recordPlayerScene } = useGLTF("record-player.glb");
 
   const models = [
     {
       scene: magnemiteScene,
-      position: [0, -0.5, -3.85],
+      position: [0, -0.15, -3.85],
       scale: [0.1, 0.1, 0.1],
       link: "https://magnemite-blond.vercel.app/",
     },
     {
       scene: iceCreamScene,
-      position: [0, -0.45, -3.85],
+      position: [0, -0.18, -3.85],
       scale: [0.1, 0.1, 0.1],
       link: "https://soft-serve-ice-cream.vercel.app/",
     },
     {
       scene: lipstickScene,
-      position: [0, -0.51, -3.85],
+      position: [0, -0.25, -3.85],
       scale: [0.028, 0.028, 0.028],
-      link: "https://lipqsticks-one.vercel.app/",
+      link: "https://lipsticks-one.vercel.app/",
+    },
+    {
+      scene: retroPhone,
+      position: [0, -0.38, -3.85],
+      scale: [0.15, 0.15, 0.15],
+      link: "https://retro-phone-theta.vercel.app/",
+    },
+    {
+      scene: recordPlayerScene,
+      position: [0, -0.25, -3.85],
+      scale: [0.25, 0.25, 0.25],
+      link: "https://record-player-jade.vercel.app/",
     },
     {
       scene: starCandyScene,
-      position: [0, -0.48, -3.85],
+      position: [0, -0.25, -3.85],
       scale: [0.12, 0.12, 0.12],
       link: "https://candy-spiral.vercel.app/",
-    },
-    {
-      scene: strawberryScene,
-      position: [0, -0.63, -3.85],
-      scale: [0.25, 0.25, 0.25],
-      link: "https://strawberry-rain.vercel.app/",
     },
   ];
 
